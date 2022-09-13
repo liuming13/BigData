7f0cb06b7ecdfa611843f09998d890bb90bcc90a
liuming13 <2107873642@qq.com>
2022-09-13 20:05:58 +0800

    2022-09-13

---
+++ com/ming/Test.java
@@ -0,0 +1,13 @@
+package com.ming;
+
+/**
+ * TODO
+ *
+ * @author Mingl lxm210787@gmail.com
+ * @date 2022/9/13 - 20:05
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
