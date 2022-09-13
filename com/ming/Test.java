6d05f8350ac736ad40d211874c08402181891dee
liuming13 <2107873642@qq.com>
2022-09-13 20:07:11 +0800

    2022-09-13

--- com/ming/Test.java
+++ com/ming/Test.java
@@ -9,5 +9,7 @@ package com.ming;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
     }
 }
